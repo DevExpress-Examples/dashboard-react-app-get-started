5250c21df685d44d4117ff72d62390bb2e805940
Sergey Nakhankov <sergey.nakhankov@devexpress.com>
2020-10-13 17:47:19 +0300

    Simplify example to match docs

--- dashboard-react-app/src/App.js
+++ dashboard-react-app/src/App.js
@@ -2,18 +2,11 @@ import React, { useState } from 'react';
 import './App.css';
 import DashboardControl from 'devexpress-dashboard-react';
 
-function switchMode(props) {
-  return props === 'Viewer' ? "Designer" : "Viewer";
-}
-
 function App() {
-  const [workingModeVar, setWorkingMode] = useState("Designer");
   return (
     <div style={{ position : 'absolute', top : '0px', left: '0px', right : '0px', bottom: '0px' }}>
-      <button onClick={() => { setWorkingMode(switchMode(workingModeVar)) }}>Switch to {switchMode(workingModeVar)}</button>
-      <DashboardControl style={{ height: '90%' }} 
-        endpoint="https://localhost:5001/dashboard/api"
-        workingMode={workingModeVar}>
+      <DashboardControl style={{ height: '100%' }} 
+        endpoint="https://localhost:5001/dashboard/api">
       </DashboardControl>
   </div>
   );
