6b8b3671c175bf17a9efb15fafb8e4c5571aa5a6
Polina Tyureva <polina.tyureva@devexpress.com>
2024-12-20 17:20:10 +0400

    update example

--- dashboard-react-app/src/App.jsx
+++ dashboard-react-app/src/App.jsx
@@ -6,7 +6,8 @@ function App() {
   return (
     <div style={{ position : 'absolute', top : '0px', left: '0px', right : '0px', bottom: '0px' }}>
       <DashboardControl style={{ height: '100%' }} 
-        endpoint="http://localhost:5000/api/dashboard">
+                endpoint="http://localhost:5000/api/dashboard">
+
       </DashboardControl>
   </div>
   );
